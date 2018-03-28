ad14e6cb9a6ac8c8070d58ecc8062ebc6b7f26fb
GalloisMax <max.gallois@outlook.fr>
2018-03-28 16:07:19 +0200

    fix(node):
    remove unnecesary html skeleton

--- server.js
+++ server.js
@@ -21,25 +21,6 @@ const html = `
     <script src="app.js"></script>
   </body>
 </html>`
-
-const hidden = `
-<!doctype html>
-<html class="no-js" lang="">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Artezic Reloaded</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>Test page Membre</h1>
-    <div id="main">
-
-    </div>
-    <script src="app.js"></script>
-  </body>
-</html>`
-
  
 app.get('/', (req, res) => {
   res.send(html)
