7d6f930d0e3d3188c108111614f505d179adcfe8
GalloisMax <max.gallois@outlook.fr>
2018-03-28 11:36:10 +0200

    filled server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const html = `
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <title>NodeJS server</title>
+    <link rel="stylesheet" href="bootstrap.min.css" />
+  </head>
+  <body>
+    <div id="main">
+
+    </div>
+    <script src="app.js"></script>
+  </body>
+</html>`
+
+app.get('/', (req, res) => {
+  res.send(html)
+  res.end()
+})
+
+app.listen(8000)
