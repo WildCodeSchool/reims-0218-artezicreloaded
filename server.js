973054e5976c5fb822dc657fcb9d6fe628b943b2
Aurelie Bayre <a.bayre@gmail.com>
2018-03-29 10:24:30 +0200

    style(server): Indented the content of const html

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ app.use(express.static('public'))
 
 
 const html = `
-<!doctype html>
-<html class="no-js" lang="">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Artezic Reloaded</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>elo Toma</h1>
-    <div id="main">
+  <!doctype html>
+  <html class="no-js" lang="">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>Artezic Reloaded</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+      <h1>elo Toma</h1>
+      <div id="main">
 
-    </div>
-    <script src="page.js"></script>
-    <script src="app.js"></script>
-  </body>
-</html>`
+      </div>
+      <script src="page.js"></script>
+      <script src="app.js"></script>
+    </body>
+  </html>`
  
 app.get('/', (req, res) => {
   res.send(html)
