c4455964dc638d24a4519face3c28800dc1cc288
Aurelie Bayre <a.bayre@gmail.com>
2018-04-11 09:36:38 +0200

    refactor: Change route name to something more coherent

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const html = `
   <body>
     <h1>Bienvenue ---nom de l'utilisateur---</h1>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Artezic</a>
+    <a class="navbar-brand" href="/">Artezic</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
