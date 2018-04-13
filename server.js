debe33f9ba77b96f775d9e289772e3ebf97a5c62
Aurelie Bayre <a.bayre@gmail.com>
2018-04-13 11:15:17 +0200

    style: ADD indent before span

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const html = `
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="/">Artezic</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
