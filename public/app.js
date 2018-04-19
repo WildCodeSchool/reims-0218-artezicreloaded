095b371f5992f117cacf093825d17b543837e647
Thib <thibaud.royer051@gmail.com>
2018-04-19 10:03:05 +0200

    indentation

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 const mainDiv = document.getElementById('main')
 
 const render = html => {
-    mainDiv.innerHTML = html
+  mainDiv.innerHTML = html
 }
 
 const makePlaylistCard = item => `
@@ -33,7 +33,7 @@ const makeCardMember = item => `
         </div>
     </div>
         `
-        const makeresultat = item => `
+const makeresultat = item => `
     <div class="card" style="width:400px">
         <img class="card-img-top" src="${item.titre}" alt="Card image">
         <div class="card-body">
@@ -45,20 +45,20 @@ const makeCardMember = item => `
         `
 
 const serializeForm = form => {
-    const data = {}
-    const elements = form.getElementsByClassName('form-control')
-    for (let el of elements) {
-        data[el.name] = el.value
-    }
-    return data
+  const data = {}
+  const elements = form.getElementsByClassName('form-control')
+  for (let el of elements) {
+    data[el.name] = el.value
+  }
+  return data
 }
 
 const controllers = {
-    '/': () => {
-        fetch('/membre/gontran')
-        .then(res => res.json())
-        .then(connectedMember => {
-            render(`
+  '/': () => {
+    fetch('/membre/gontran')
+      .then(res => res.json())
+      .then(connectedMember => {
+        render(`
             <h1>Bienvenue ${connectedMember[0].pseudo}</h1>
             <h2>Ce que vous pouvez faire:</h2>
             <ul>
@@ -69,25 +69,25 @@ const controllers = {
                     Consulter la page membre
                 </li>
             </ul>`)
-       }) 
-        
-    },
-    '/monprofil': () => {
-        fetch('/membre/gontran')
-        .then(res => res.json())
-        //TODO we don't need a reduce here because we're gettingonly one object, not severals.
-        .then(membre => makeCardMember(membre[0]))
-        .then(mesInfos=> render(
-            `<div class="row">
+      })
+
+  },
+  '/monprofil': () => {
+    fetch('/membre/gontran')
+      .then(res => res.json())
+      //TODO we don't need a reduce here because we're gettingonly one object, not severals.
+      .then(membre => makeCardMember(membre[0]))
+      .then(mesInfos => render(
+        `<div class="row">
                 ${mesInfos}
             </div>
             <p><a class="btn btn-success btn-lg" href="/editer-mon-profil" role="button">Editer mon profil</a></p>
             <p><a class="btn btn-success btn-lg" href="/newplaylist" role="button">Ajouter une playlist »</a></p>
             `
-        ))
-    },
-    '/editer-mon-profil' : () => {
-        render(`
+      ))
+  },
+  '/editer-mon-profil': () => {
+    render(`
         <div class="container">
             <div id="alert-box" class="hidden">
             </div>
@@ -106,132 +106,128 @@ const controllers = {
             </form>
         </div>
         `)
-        const formProfile = document.getElementById('editMyProfile')
-        formProfile.addEventListener('submit', e => {
-            e.preventDefault()
-            const dataProfile = serializeForm(formProfile)
-            fetch('/membres', {
-                method: 'PUT',
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(dataProfile) 
-            })
-            .then(res => res.json())
-            .then(wilderEdition => {
-                const alertBox = document.getElementById('alert-box')
-                alertBox.className = 'alert alert-success'
-                alertBox.innerHTML = `Votre profil titre été édité`
-            })
+    const formProfile = document.getElementById('editMyProfile')
+    formProfile.addEventListener('submit', e => {
+      e.preventDefault()
+      const dataProfile = serializeForm(formProfile)
+      fetch('/membres', {
+          method: 'PUT',
+          headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dataProfile)
         })
-    },   
-    '/newplaylist': () => {
-        render(`
-        <div class="container">
-          <div id="alert-box" class="hidden">
-          </div>
-          <form id="add-playlist">
-            <div class="form-group">
-              <label for="inputTitle">Titre</label>
-              <input name="title" type="text" class="form-control" id="inputTitle" placeholder="Entrez le titre de votre playlist">
-            </div>
-            <div class="form-group">
-              <label for="inputGenre">Genre musical</label>
-              <input name="genre" type="text" class="form-control" id="inputGenre" placeholder="Quel est le genre de votre playlist ?">
-            </div>
-            <div class="form-group">
-                <label for="inputUrl">Url de votre playlist</label>
-                <input name="url" type="text" class="form-control" id="inputUrl" placeholder="Entrez l'url de votre playlist">
-            </div>
-            <div class="form-group">
-                <label for="competition">Concourir avec cette playlist?</label>
-                <input name="competition" type="radio" id="competition" class="form-control" value="true">
-            </div>        
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-          <a class="btn btn-success btn-lg" href="/" role="button">retour page d'accueil</a>
-        </div>`
-        )
-        const form = document.getElementById('add-playlist')
-        form.addEventListener('submit', e => {
-            e.preventDefault() 
-            const data = serializeForm(form)
-            const dataWithId = {
-                titre: data.title,
-                genre: data.genre,
-                url: data.url,
-                compete: data.competition,
-                id_wilders:1
-            } 
-            
-            fetch('/playlists', {
-                method: 'POST',
-                headers: {
-                    
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(dataWithId) // le corps de ma requête est mon objet data jsonifié. car sqlite fonctionne en json
-            })
-            .then(res => res.json())
-            .then(playlist => {
-                const alertBox = document.getElementById('alert-box')
-                alertBox.className = 'alert alert-success'
-                alertBox.innerHTML = `Votre playlist titre ${playlist.titre} (${playlist.id}) a bien été créée`
-            })
-        })
-    },
-    '/wilders': () => {
-        fetch('/membres')
         .then(res => res.json())
-        .then(listusers => listusers.reduce((carry, user) => carry + makeWilder(user), ''))
-        .then(book => render(
-            `
+        .then(wilderEdition => {
+          const alertBox = document.getElementById('alert-box')
+          alertBox.className = 'alert alert-success'
+          alertBox.innerHTML = `Votre profil titre été édité`
+        })
+    })
+  },
+  '/newplaylist': () => {
+    render(`
+        <div class="container">
+          <div id="alert-box" class="hidden"></div>
+            <form id="add-playlist">
+              <div class="form-group">
+                <label for="inputTitle">Titre</label>
+                  <input name="title" type="text" class="form-control" id="inputTitle" placeholder="Entrez le titre de votre playlist">
+              </div>
+              <div class="form-group">
+                <label for="inputGenre">Genre musical</label>
+                  <input name="genre" type="text" class="form-control" id="inputGenre" placeholder="Quel est le genre de votre playlist ?">
+              </div>
+              <div class="form-group">
+                  <label for="inputUrl">Url de votre playlist</label>
+                    <input name="url" type="text" class="form-control" id="inputUrl" placeholder="Entrez l'url de votre playlist">
+              </div>
+              <div class="form-group">
+                  <label for="competition">Concourir avec cette playlist?</label>
+                    <input name="competition" type="radio" id="competition" class="form-control" value="true">
+              </div>        
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+              <a class="btn btn-success btn-lg" href="/" role="button">retour page d'accueil</a>
+        </div>`)
+    const form = document.getElementById('add-playlist')
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      const data = serializeForm(form)
+      const dataWithId = {
+        titre: data.title,
+        genre: data.genre,
+        url: data.url,
+        compete: data.competition,
+        id_wilders: 1
+      }
+
+      fetch('/playlists', {
+          method: 'POST',
+          headers: {
+
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dataWithId) // le corps de ma requête est mon objet data jsonifié. car sqlite fonctionne en json
+        })
+        .then(res => res.json())
+        .then(playlist => {
+          const alertBox = document.getElementById('alert-box')
+          alertBox.className = 'alert alert-success'
+          alertBox.innerHTML = `Votre playlist titre ${playlist.titre} (${playlist.id}) a bien été créée`
+        })
+    })
+  },
+  '/wilders': () => {
+    fetch('/membres')
+      .then(res => res.json())
+      .then(listusers => listusers.reduce((carry, user) => carry + makeWilder(user), ''))
+      .then(book => render(
+        `
             <div class="row">
             ${book}
             </div>
-            `)
-        )
-    },
-    '/viewplaylists/:slug': ctx => {
-        const { slug } = ctx.params
-        //hardcoding fetch route but we should be able to get idwilder from slug?
-        fetch(`/membre/${slug}`)
-        .then(res => res.json())
-        .then(gontran => {
-            const playlists = gontran[0].playlists
-            const gontranPlaylistsCards = playlists.reduce((acc, playlist) => acc + makePlaylistCard(playlist), '')
-            render(`
+            `))
+  },
+  '/viewplaylists/:slug': ctx => {
+    const {
+      slug
+    } = ctx.params
+    //hardcoding fetch route but we should be able to get idwilder from slug?
+    fetch(`/membre/${slug}`)
+      .then(res => res.json())
+      .then(gontran => {
+        const playlists = gontran[0].playlists
+        const gontranPlaylistsCards = playlists.reduce((acc, playlist) => acc + makePlaylistCard(playlist), '')
+        render(`
             <h2>Hello ${slug}, voici vos playlists:</h2>
             <div class="row>
                 ${gontranPlaylistsCards}
             </div>`)
-        }
-      )
-    },
-    '/concours': () => {
-        fetch('/playlists')
-        .then(res => res.json())
-        .then(result => result.reduce((carry, user) => carry + makeresultat(user), ''))
-        .then(book => render(
-            `
+      })
+  },
+  '/concours': () => {
+    fetch('/playlists')
+      .then(res => res.json())
+      .then(result => result.reduce((carry, user) => carry + makeresultat(user), ''))
+      .then(book => render(
+        `
             <div class="row">
             ${book}
             </div>
-            `)
-        )
-    }
+            `))
+  }
 }
 
 
-const route = pathname => {
-}
+const route = pathname => {}
 
 (() => {
-    ['/', '/wilders', '/monprofil', '/newplaylist', '/editer-mon-profil', '/viewplaylists/:slug', '/concours'].forEach(
-        path => page(path, controllers[path])
-    )
-    page()
-    // route()
+  ['/', '/wilders', '/monprofil', '/newplaylist', '/editer-mon-profil', '/viewplaylists/:slug', '/concours'].forEach(
+    path => page(path, controllers[path])
+  )
+  page()
+  // route()
 })()
