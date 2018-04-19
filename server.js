4bd7198e85ee83e0e5a405ae5855c79a72b4b954
Thib <thibaud.royer051@gmail.com>
2018-04-19 09:46:50 +0200

    update 19.04

--- server.js
+++ server.js
@@ -2,48 +2,48 @@ console.log("je suis le serveur, je commence")
 const sqlite = require('sqlite')
 const express = require('express')
 const Promise = require('bluebird')
-const app = express() 
+const app = express()
 
-const users = require('./public/user-playlists.json') 
+const users = require('./public/user-playlists.json')
 const bodyParser = require('body-parser')
-let db 
+let db
 
-app.use(express.static('public')) 
+app.use(express.static('public'))
 app.use(bodyParser.json())
 
 const insertWilder = w => {
   const { pseudo, bio, avatar } = w
-  return db.get('INSERT INTO wilders(pseudo, bio, avatar) VALUES(?, ?, ?)', pseudo, bio, avatar) 
-  .then(() => db.get('SELECT last_insert_rowid() as id')) 
-  .then(({id})=> db.get('SELECT * from wilders WHERE id = ?', id)) 
+  return db.get('INSERT INTO wilders(pseudo, bio, avatar) VALUES(?, ?, ?)', pseudo, bio, avatar)
+    .then(() => db.get('SELECT last_insert_rowid() as id'))
+    .then(({ id }) => db.get('SELECT * from wilders WHERE id = ?', id))
 }
 
 const insertPlaylist = w => {
   const { titre, genre, url, id_wilders, compete, nbrevotes } = w
   return db.get('INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes) VALUES(?, ?, ?, ?, ?, ?)', titre, genre, url, id_wilders, compete, nbrevotes) // On retourne une méthode (db.get) On passe une requête MYSQL qui prend le titre, url et genre
-  .then(() => db.get('SELECT last_insert_rowid() as id')) 
-  .then(({id})=> db.get('SELECT * from playlists WHERE id = ?', id))
+    .then(() => db.get('SELECT last_insert_rowid() as id'))
+    .then(({ id }) => db.get('SELECT * from playlists WHERE id = ?', id))
 }
 
 const modifyMyProfile = newInfo => {
   const { pseudo, bio } = newInfo
   console.log('pseudo is ', pseudo, 'and bio is : ', bio)
   return db.get('UPDATE wilders SET pseudo=?, bio=? WHERE id=1', pseudo, bio)
-  .then(()=> db.get('SELECT * from wilders WHERE ID=1'))
+    .then(() => db.get('SELECT * from wilders WHERE ID=1'))
 }
 
-const dbPromise = Promise.resolve() 
-  .then(() => sqlite.open('./database.sqlite', {Promise}))
+const dbPromise = Promise.resolve()
+  .then(() => sqlite.open('./database.sqlite', { Promise }))
   .then(_db => {
     db = _db
-  return db.migrate({force: 'last'})
-})
-.then(() => {
-  Promise.map(users, w => {
-    insertWilder(w)
+    return db.migrate({ force: 'last' })
+  })
+  .then(() => {
+    Promise.map(users, w => {
+      insertWilder(w)
     })
   }
-) 
+  )
 
 const html = `
 <!doctype html>
@@ -65,7 +65,7 @@ const html = `
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" href="/monprofil">Mon profil<span class="sr-only">(current)</span></a>
-        </li>
+        </li>rg
         <li class="nav-item">
           <a class="nav-link" href="/wilders">Equipe</a>
         </li>
@@ -90,47 +90,47 @@ app.get('/', (req, res) => {
   res.end()
 })
 
-app.post('/membres', (req, res) => { 
+app.post('/membres', (req, res) => {
   return insertWilder(req.body)
-  .then(recordNewWilder => {
-    res.json(recordNewWilder)
-  }) 
+    .then(recordNewWilder => {
+      res.json(recordNewWilder)
+    })
 })
 
-app.get('/membre', (req, res) => { 
+app.get('/membre', (req, res) => {
   db.all("SELECT * from wilders WHERE id='1'")
-  .then(oneWilder => {
-    res.json(oneWilder)
-  })
+    .then(oneWilder => {
+      res.json(oneWilder)
+    })
 })
 
 
-app.get('/membres', (req, res) => { 
+app.get('/membres', (req, res) => {
   db.all('SELECT * from wilders')
-  .then(allWilders => {
-    res.json(allWilders)
-  })
+    .then(allWilders => {
+      res.json(allWilders)
+    })
 })
 
-app.post('/playlists', (req, res) => { 
+app.post('/playlists', (req, res) => {
   return insertPlaylist(req.body)
-  .then(recordNewPlaylist => {
-    res.json(recordNewPlaylist)
-  }) 
+    .then(recordNewPlaylist => {
+      res.json(recordNewPlaylist)
+    })
 })
 
 app.put('/membres', (req, res) => {
   return modifyMyProfile(req.body)
-  .then(wilderIsEdited => {
-    res.json(wilderIsEdited)
-  })
+    .then(wilderIsEdited => {
+      res.json(wilderIsEdited)
+    })
 })
 
-app.get('/playlists', (req, res) => { 
+app.get('/playlists', (req, res) => {
   db.all('SELECT * from playlists')
-  .then(allPlaylists => {
-    res.json(allPlaylists)
-  })
+    .then(allPlaylists => {
+      res.json(allPlaylists)
+    })
 })
 
 app.get('*', (req, res) => {
