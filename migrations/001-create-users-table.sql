2815a566bf0b165718a71ecd6909195d61ea2533
Aurelie Bayre <a.bayre@gmail.com>
2018-04-19 16:14:27 +0200

    refactor: add more url examples

--- migrations/001-create-users-table.sql
+++ migrations/001-create-users-table.sql
@@ -20,14 +20,14 @@ FOREIGN KEY(id_wilders) REFERENCES wilders(id)
 );
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('J''aime le rock', 'rock', 'www.truc.com', 1, 'true', 17);
+VALUES ('J''aime le rock', 'rock', 'https://play.soundsgood.co/playlist/gontran-aime-le-rock', 1, 'true', 17);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('Gontran the best', 'rap', 'www.blu.com', 1, 'false', 2);
+VALUES ('Gontran the best', 'rap', 'https://play.soundsgood.co/playlist/gontran-la-creme-of-the-creme', 1, 'false', 2);
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('SQL le retour', 'chill', 'www.sqlite.com', 2, 'false', 8);
+VALUES ('SQL le retour', 'chill', 'https://youtu.be/QjICgmk31js', 2, 'false', 8);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('linux forever', 'slow', 'www.linux.com', 2, 'true', 15);
+VALUES ('linux forever', 'slow', 'https://youtu.be/bju_FdCo42w', 2, 'true', 15);
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
 VALUES ('Concentration pour coder', 'musique bizarre', 'https://youtu.be/0r6C3z3TEKw', 3, 'false', 5);
