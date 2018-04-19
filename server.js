15caa0dfbfdda9e2ef58a62d5f5f5ca801d1086a
Aurelie Bayre <a.bayre@gmail.com>
2018-04-19 11:13:56 +0200

    style: add indentation

--- server.js
+++ server.js
@@ -106,12 +106,12 @@ app.post('/membres', (req, res) => {
 
 app.get('/connected', (req, res) => { 
     db.all(`
-    SELECT wilders.id as wilderId, playlists.id as playlistId, pseudo, avatar, bio, titre, genre, url, compete, nbrevotes
-    from wilders
-    left join playlists on wilders.id = playlists.id_wilders
-    WHERE id_wilders = 1
-    ; 
-    `)
+      SELECT wilders.id as wilderId, playlists.id as playlistId, pseudo, avatar, bio, titre, genre, url, compete, nbrevotes
+      from wilders
+      left join playlists on wilders.id = playlists.id_wilders
+      WHERE id_wilders = 1
+      ; 
+      `)
   .then(wilderPlaylists => {
     res.json(wildersWithPlaylists(wilderPlaylists))
   })
