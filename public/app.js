01ffb073cabb39f06d86c55647a87552e84310c8
Aurelie Bayre <a.bayre@gmail.com>
2018-04-19 16:26:47 +0200

    feat: add modal template

--- public/app.js
+++ public/app.js
@@ -10,6 +10,26 @@ const makePlaylistCard = item => `
             <h5 class="card-title">${item.titre}</h5>
             <p class="card-text">${item.genre}</p>
             <a href="${item.url}" class="btn btn-primary">Voir ma playlist</a>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Lancer ma playlist
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${item.titre}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                </div>
+            </div>
+            </div>
         </div>
     </div>
     `
