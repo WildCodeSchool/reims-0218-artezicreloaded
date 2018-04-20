4669dd012f7c1ba75489858a2b1852bcbe237571
Aurelie Bayre <a.bayre@gmail.com>
2018-04-20 11:40:35 +0200

    fix: fix prop to call to display winning playlist url

--- public/app.js
+++ public/app.js
@@ -4,35 +4,39 @@ const render = html => {
   mainDiv.innerHTML = html
 }
 
-const makePlaylistCard = item => `
-<div class="col-12 col-sm-12 col-md-3">
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${item.titre}</h5>
-            <p class="card-text">${item.genre}</p>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal${item.playlistId}">
-            Lancer ma playlist
-            </button>
-
-            <!-- Modal -->
-            <div class="modal fade" id="modal${item.playlistId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">${item.titre}</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <iframe width="480" height="270" src="${item.url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+const makePlaylistCard = item => {
+    console.log(item)
+    return 
+    `
+    <div class="col-12 col-sm-12 col-md-3">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${item.titre}</h5>
+                <p class="card-text">${item.genre}</p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal${item.playlistId}">
+                Lancer ma playlist
+                </button>
+    
+                <!-- Modal -->
+                <div class="modal fade" id="modal${item.playlistId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">${item.titre}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <iframe width="480" height="270" src="${item.url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    `
+        `
+} 
 const makeWilder = item => `
 <div class="col-12 col-sm-12 col-md-3">
   <div class="card-deck" >
@@ -60,18 +64,39 @@ const makeCardMember = item => `
     </div>
         `
 
-const makeWinningCard = item => `
+const makeWinningCard = item => {
+    console.log(item)
+    return `
     <div class="col-12 col-sm-12 col-md-4">
         <div class="card" style="width:400px">
             <img class="card-img-top" src="https://png.pngtree.com/element_origin_min_pic/17/07/23/473f204a1589862d0264b14f926b4b59.jpg" alt="Card image">
             <div class="card-body">
                 <h4 class="card-title">${item.playlists[0].titre}</h4>
                 <p class="card-text">${item.playlists[0].nbrevotes} votes</p>
-                <a href="https://${item.playlists[0].url}" target="_blank" class="btn btn-primary">Voir la playlist</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal${item.playlistId}">
+            Lancer ma playlist
+            </button>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="modal${item.playlistId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">${item.titre}</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <iframe width="480" height="270" src="${item.playlists[0].url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
       </div>
-        `
+        `}
 
 const serializeForm = form => {
   const data = {}
