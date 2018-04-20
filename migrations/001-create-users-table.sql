b4fd70a958cf2c2df545878211dd652dc392476b
Aurelie Bayre <a.bayre@gmail.com>
2018-04-20 09:46:25 +0200

    fix: Change to soundsgood url only, for the demo

--- migrations/001-create-users-table.sql
+++ migrations/001-create-users-table.sql
@@ -20,26 +20,26 @@ FOREIGN KEY(id_wilders) REFERENCES wilders(id)
 );
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('SQL le retour', 'chill', 'https://youtu.be/embed/QjICgmk31js', 1, 'false', 8);
+VALUES ('SQL le retour', 'chill', 'https://play.soundsgood.co/playlist/sqlite', 1, 'false', 8);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
 VALUES ('J''aime le rock', 'rock', 'https://play.soundsgood.co/playlist/gontran-aime-le-rock', 1, 'true', 17);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
 VALUES ('Gontran the best', 'rap', 'https://play.soundsgood.co/playlist/gontran-la-creme-of-the-creme', 1, 'false', 2);
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('SQL le retour', 'chill', 'https://youtu.be/embed/QjICgmk31js', 2, 'false', 8);
+VALUES ('Node', 'chill', 'https://play.soundsgood.co/playlist/node', 2, 'false', 8);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('linux forever', 'slow', 'https://youtu.be/embed/bju_FdCo42w', 2, 'true', 15);
+VALUES ('linux forever', 'slow', 'https://play.soundsgood.co/playlist/linux', 2, 'true', 15);
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('Concentration pour coder', 'musique bizarre', 'https://youtu.be/embed/0r6C3z3TEKw', 3, 'false', 5);
+VALUES ('Concentration pour coder', 'musique bizarre', 'https://play.soundsgood.co/playlist/concentration-4', 3, 'false', 5);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('Pugs life', 'relax your pug', 'https://youtu.be/embed/xSH7LvjHDiA', 3, 'true', 11);
+VALUES ('Pugs life', 'relax your pug', 'https://play.soundsgood.co/playlist/pugs-life', 3, 'true', 11);
 
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
 VALUES ('node', 'serveur', 'https://play.soundsgood.co/playlist/example', 4, 'false', 6);
 INSERT INTO playlists(titre, genre, url, id_wilders, compete, nbrevotes)
-VALUES ('NPM', 'rap français', 'https://youtu.be/embed/biYdUZXfz9I', 4, 'true', 7);
+VALUES ('NPM', 'rap français', 'https://play.soundsgood.co/playlist/ntm', 4, 'true', 7);
 
 CREATE TABLE competitions (
 id INTEGER PRIMARY KEY,
