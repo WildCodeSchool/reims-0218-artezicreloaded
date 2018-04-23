ead04ef277573f773af99c12f206693df8a6ac92
GalloisMax <max.gallois@outlook.fr>
2018-04-23 17:43:17 +0200

    footer indentation

--- server.js
+++ server.js
@@ -78,62 +78,51 @@ const dbPromise = Promise.resolve()
   })
 
 const html = `
-  <!doctype html>
-  <html class="no-js" lang="">
-    <head>
-      <meta charset="utf-8">
-      <link rel="stylesheet" href="style.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Artezic Reloaded</title>
-      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head>
-    <body>
-      <div class="container">
-        <div class="row">
-          <nav class="navbar navbar-expand-lg navbar-light bg-light col-12 col-sm-12 col-md-12"">
-            <a class="navbar-brand" href="/">Artezic</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="/monprofil">Mon profil<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/wilders">Equipe</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/concours">Concours</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <div id="carouselSlidesOnly" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="http://res.cloudinary.com/dlfnke6kc/image/upload/v1524129058/artezik_2_reloaded_zfn4l4.jpg" alt="Artezik2 reloaded">
-              </div>
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Artezic Reloaded</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light col-12 col-sm-12 col-md-12"">
+          <a class="navbar-brand" href="/">Artezic</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="/monprofil">Mon profil<span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/wilders">Equipe</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/concours">Concours</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div id="carouselSlidesOnly" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="http://res.cloudinary.com/dlfnke6kc/image/upload/v1524129058/artezik_2_reloaded_zfn4l4.jpg" alt="Artezik2 reloaded">
             </div>
           </div>
-        <div id="main">
-
         </div>
-      </div>
-      <script src="/page.js"></script>
-      <script src="/app.js"></script>
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+      <div id="main">
 
-<!-- Footer -->
+      </div>
+    </div>
+    <script src="/page.js"></script>
+    <script src="/app.js"></script>
+  </body>
 	<section id="footer">
 		<div class="container">
 			<div class="row text-center text-xs-center text-sm-left text-md-left">
@@ -169,7 +158,6 @@ const html = `
 				</div>
 			</div>
       <div class="container">
-              <a href='#'><i class="fa fa-twitch fa-3x fa-fw"></i></a>
               <a href='#'><i class="fa fa-facebook fa-3x fa-fw"></i></a>
               <a href='#'><i class="fa fa-twitter fa-3x fa-fw"></i></a>
               <a href='#'><i class="fa fa-youtube-play fa-3x fa-fw"></i></a>
@@ -177,7 +165,6 @@ const html = `
               <a href='#'><i class="fa fa-vine fa-3x fa-fw"></i></a>
               <a href='#'><i class="fa fa-flickr fa-3x fa-fw"></i></a>
               <a href='#'><i class="fa fa-linkedin fa-3x fa-fw"></i></a>
-            </span>
       </div>	
 			<div class="row">
 				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
