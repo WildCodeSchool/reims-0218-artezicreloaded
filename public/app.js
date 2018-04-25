7b57a10846a64fc1e9bf2eb96596f53472cb910f
GalloisMax <max.gallois@outlook.fr>
2018-04-25 11:58:29 +0200

    formulaire new generation du live coding du 25 avril

--- public/app.js
+++ public/app.js
@@ -83,6 +83,8 @@ const serializeForm = form => {
   return data
 }
 
+const token = localStorage.getItem('token')
+
 const controllers = {
     '/': () => {
         let resultPlaylistCompete
@@ -239,7 +241,88 @@ const controllers = {
         })
     },
     '/authentification': () => {
+        
+        const loginFormHtml = `<form id="loginForm">
+<input class="form-control" name="username" placeholder="username"/>
+<input class="form-control" type="password" name="password" placeholder="password"/>
+<input type="submit" value="se connecter" />
+</form>`
+
+
+
+render(`
+    <div id="alert-login"></div> 
+    ${ !token ? loginFormHtml : '<button id="disconnect" type="button">se deconnecter</button>' }
+    <div id="test">CALL TEST</div>
+`)
+
+if (!token) {
+    const loginForm = document.getElementById('loginForm')
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault()
+        //data ?
+        const data = serializeForm(loginForm)
+        console.log(data)
+        //post sur le server /auth/login
+        fetch('/auth/login', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            const alert = document.getElementById('alert-login')
+            if(!data.user) {
+                //alert class danger
+                alert.innerHTML = `echec`
+            } else {
+                alert.innerHTML = `${data.user.username} est connecté <button id="disconnect" type="button">se deconnecter</button>`
+                //stores the token
+                localStorage.setItem('token', data.token)
+                loginForm.style.display = 'none'
+                document.getElementById('disconnect').addEventListener('click', () => {
+                    localStorage.removeItem('token')
+                    render(`
+                        <div id="alert-login"></div> 
+                        ${ loginFormHtml }
+                        <div id="test">CALL TEST</div>
+                    `)
+                })
+            }
+        })
+    })
+
+} else {
+    document.getElementById('disconnect').addEventListener('click', () => {
+        localStorage.removeItem('token')
         render(`
+            <div id="alert-login"></div> 
+            ${ loginFormHtml }
+            <div id="test">CALL TEST</div>
+        `)
+    })
+}
+
+document.getElementById('test').addEventListener('click', () => {
+    const token = localStorage.getItem('token')
+    console.log(token)
+    fetch('test',{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,          
+        }
+    })
+    .then(res => res.json())
+    .catch(err => console.log(err))
+})
+
+
+    /*    render(`
             <div class="container">
             <div id="alert-box" class="hidden">
             </div>
@@ -267,9 +350,9 @@ const controllers = {
         }
         fetch('/conection', {
             method: 'POST',
-            headers: {
-                'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
+            headers: { */
+//                'Accept': 'application/json, text/plain, */*',
+    /*            'Content-Type': 'application/json'
             },
             body: JSON.stringify(dataWithId)
             })
@@ -279,7 +362,7 @@ const controllers = {
             alertBox.className = 'alert alert-success'
             alertBox.innerHTML = `Vous êtes connecté ${playlist.nameuser} (${playlist.id}) bonne écoute`
             })
-        })
+        })*/
     },
     '/wilders': () => {
         fetch('/membres')
