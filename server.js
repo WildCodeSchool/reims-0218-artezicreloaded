1727c1090fcf5aa6d8509774ad45665518c3ed1e
GalloisMax <max.gallois@outlook.fr>
2018-05-02 15:06:47 +0200

    footer prets pour etre integrer

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ const html = `
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Artezic Reloaded</title>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="/style.css">
     </head>
     <body>
       <div class="container">
